160963639b6b38fcb2c7a4e4b0824389e5588b67
yusam <yusam@yandex.ru>
2025-01-28 22:42:36 +0400

    fix

--- Assets/Plugins/AtomicGame/Entity/Content/CoinArea/CoinAreaBehaviour.cs
+++ Assets/Plugins/AtomicGame/Entity/Content/CoinArea/CoinAreaBehaviour.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 
 namespace AtomicGame
 {
-    public sealed class CoinAreaBehaviour : IEntityInit, IEntityDispose
+    public sealed class CoinAreaBehaviour : IEntityInit, IEntityDispose, IEntityUpdate
     {
         private GameContext _gameContext;
         private Transform _transform;
+        private bool _isRotate;
+        private float _rotateSpeed;
         
         private TriggerDispatcher _trigger;
 
-        public CoinAreaBehaviour()
+        public CoinAreaBehaviour(bool isRotate, float rotateSpeed)
         {
-     
+            _isRotate = isRotate;
+            _rotateSpeed = rotateSpeed;
         }
 
         public void Init(in IEntity entity)
@@ -35,6 +38,14 @@ namespace AtomicGame
             IReactiveVariable<int> score = _gameContext.GetScore();
             score.Value += 1;
             _transform.gameObject.SetActive(false);
+        }
+
+        public void OnUpdate(in IEntity entity, in float deltaTime)
+        {
+            if (_isRotate)
+            {
+                _transform.Rotate(Vector3.up, 1 * _rotateSpeed * deltaTime);
+            }
         }
     }
 }
--- Assets/Plugins/AtomicGame/Entity/Content/CoinArea/CoinAreaInstaller.cs
+++ Assets/Plugins/AtomicGame/Entity/Content/CoinArea/CoinAreaInstaller.cs
@@ -10,11 +10,17 @@ namespace AtomicGame
         [SerializeField]
         private TriggerDispatcher _trigger;
 
+        [SerializeField]
+        private bool _isRotate;
+        
+        [SerializeField]
+        private float _rotateSpeed = 200f;
+        
         public override void Install(IEntity entity)
         {
             entity.AddTransform(transform);
             entity.AddTriggerDispatcher(_trigger);
-            entity.AddBehaviour(new CoinAreaBehaviour());
+            entity.AddBehaviour(new CoinAreaBehaviour(_isRotate, _rotateSpeed));
         }
     }
 }
