a7dc21e398278cade1c819f33ad26245e73dbf08
yusam <yusam@yandex.ru>
2025-02-06 14:37:26 +0400

    changed

--- Assets/Plugins/AtomicGame/GameContext/Controllers/MenuInGame/MenuInGameController.cs
+++ Assets/Plugins/AtomicGame/GameContext/Controllers/MenuInGame/MenuInGameController.cs
@@ -31,6 +31,13 @@ namespace AtomicGame
                 _gameContextMenuInGamePresenter.volumeSlider.value = context.GetAudioVolume().Value;
                 _gameContextMenuInGamePresenter.musicSlider.value = context.GetAudioMusic().Value;
                 _gameContextMenuInGamePresenter.effectSlider.value = context.GetAudioEffect().Value;
+            } else
+            if (InputUseCase.IsPause(_playerContext) && context.GetGameState().Value != GameContextState.statePlay)
+            {
+                context.GetGameState().Value = GameContextState.statePlay;
+                _gameContextMenuInGamePresenter.gameObject.SetActive(false);  
+                ApplicationHelper.HideCursor();       
+                ApplicationHelper.Resume();
             }
         }
     }
